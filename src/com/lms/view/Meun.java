6d0f306f55d9d69cf8899c27d9d0bf294317b51b
unknown <2250047157@qq.com>
2018-05-26 18:01:42 +0800

    修改模板

--- src/com/lms/view/Meun.java
+++ src/com/lms/view/Meun.java
@@ -9,6 +9,10 @@ import javax.swing.ImageIcon;
 import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JDesktopPane;
+import java.awt.BorderLayout;
 
 public class Meun {
 
@@ -23,6 +27,8 @@ public class Meun {
 				try {
 					Meun window = new Meun();
 					window.frame.setVisible(true);
+					window.frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+//					setExtendedState(JFrame.MAXIMIZED_BOTH);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -43,57 +49,62 @@ public class Meun {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.WHITE);
-		frame.setBounds(100, 100, 930, 540);
+		frame.setBounds(100, 100, 1025, 682);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		
 		
 		JLabel lblNewLabel = new JLabel("\u6B22\u8FCE\u4F7F\u7528\u56FE\u4E66\u7BA1\u7406\u7CFB\u7EDF");
 		lblNewLabel.setFont(new Font("ËÎÌו", Font.BOLD, 20));
 		lblNewLabel.setIcon(new ImageIcon(Meun.class.getResource("/icon/title.png")));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(305, 15, 286, 57);
+		lblNewLabel.setBounds(328, 86, 286, 57);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JButton jb_BookManager = new JButton("\u56FE\u4E66\u7EF4\u62A4");
 		jb_BookManager.setHorizontalAlignment(SwingConstants.LEADING);
 		jb_BookManager.setIcon(new ImageIcon(Meun.class.getResource("/icon/book.png")));
 		jb_BookManager.setBackground(Color.WHITE);
-		jb_BookManager.setBounds(133, 135, 199, 50);
+		jb_BookManager.setBounds(170, 214, 199, 50);
 		frame.getContentPane().add(jb_BookManager);
 		
 		JButton jb_UserManager = new JButton("\u7528\u6237\u7EF4\u62A4");
+		
 		jb_UserManager.setIcon(new ImageIcon(Meun.class.getResource("/icon/user-set.png")));
 		jb_UserManager.setHorizontalAlignment(SwingConstants.LEADING);
 		jb_UserManager.setBackground(Color.WHITE);
-		jb_UserManager.setBounds(552, 135, 199, 50);
+		jb_UserManager.setBounds(589, 214, 199, 50);
 		frame.getContentPane().add(jb_UserManager);
 		
 		JButton jb_Brorrow = new JButton("\u501F    \u4E66");
 		jb_Brorrow.setIcon(new ImageIcon(Meun.class.getResource("/icon/borrow.png")));
 		jb_Brorrow.setHorizontalAlignment(SwingConstants.LEADING);
 		jb_Brorrow.setBackground(Color.WHITE);
-		jb_Brorrow.setBounds(133, 228, 199, 50);
+		jb_Brorrow.setBounds(170, 307, 199, 50);
 		frame.getContentPane().add(jb_Brorrow);
 		
 		JButton jb_Back = new JButton("\u8FD8    \u4E66");
 		jb_Back.setIcon(new ImageIcon(Meun.class.getResource("/icon/back.png")));
 		jb_Back.setHorizontalAlignment(SwingConstants.LEADING);
 		jb_Back.setBackground(Color.WHITE);
-		jb_Back.setBounds(552, 228, 199, 50);
+		jb_Back.setBounds(589, 307, 199, 50);
 		frame.getContentPane().add(jb_Back);
 		
 		JButton jb_Search = new JButton("\u4FE1\u606F\u67E5\u8BE2");
 		jb_Search.setIcon(new ImageIcon(Meun.class.getResource("/icon/search.png")));
 		jb_Search.setHorizontalAlignment(SwingConstants.LEADING);
 		jb_Search.setBackground(Color.WHITE);
-		jb_Search.setBounds(133, 337, 199, 50);
+		jb_Search.setBounds(170, 416, 199, 50);
 		frame.getContentPane().add(jb_Search);
 		
 		JButton jb_Exit = new JButton("\u9000    \u51FA");
 		jb_Exit.setIcon(new ImageIcon(Meun.class.getResource("/icon/exit.png")));
 		jb_Exit.setHorizontalAlignment(SwingConstants.LEADING);
 		jb_Exit.setBackground(Color.WHITE);
-		jb_Exit.setBounds(552, 337, 199, 50);
+		jb_Exit.setBounds(589, 416, 199, 50);
 		frame.getContentPane().add(jb_Exit);
+		
+		
 	}
 }
